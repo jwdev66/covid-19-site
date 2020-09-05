583cc948010356da82e9936a63eb87b8d653357b
jwdev66 <jwdev66@gmail.com>
2020-09-05 23:56:52 +0300

    :docs: added ConrolSelect component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ControlSelect from './ControlSelect/ControlSelect';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Covid 19 tracker</h1>
+      <ControlSelect />
+    </div>
+  );
+};
+
+export default App;
