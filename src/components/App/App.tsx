580ea0d978b0767524422913eced7e60010be402
jwdev66 <jwdev66@gmail.com>
2020-09-07 20:00:13 +0300

    :docs and refactor: create component CovidStatistix and transfer InfoBoxes components from App component to CovidStatistics, added media query styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
 import ControlSelect from '../ControlSelect/ControlSelect';
-import InfoBox from '../InfoBox/InfoBox';
-import { getCountry } from '../../store/selectors/covidSelectors';
 import { Cases } from '../../types';
 import Map from '../Map/Map';
+import CovidInformation from '../CovidInformation/CovidInformation';
+import CovidStatistics from '../CovidStatistics/CovidStatistics';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
-import CovidInformation from '../CovidInformation/CovidInformation';
 
 const App: React.FC = () => {
-  const country = useSelector(getCountry);
   const [cases, setCases] = useState<Cases>('cases');
+
+  const setCasesState = (cases: Cases): void => {
+    setCases(cases);
+  };
 
   return (
     <div className="App">
@@ -20,31 +21,7 @@ const App: React.FC = () => {
           <h1>COVID-19 Tracker</h1>
           <ControlSelect />
         </div>
-        <div className="App__stats">
-          <InfoBox
-            onClick={() => setCases('cases')}
-            title="Coronavirus Cases"
-            cases={country.todayCases}
-            total={country.cases}
-            isRed
-            active={cases === 'cases'}
-          />
-          <InfoBox
-            onClick={() => setCases('recovered')}
-            title="Recovered"
-            cases={country.todayRecovered}
-            total={country.recovered}
-            active={cases === 'recovered'}
-          />
-          <InfoBox
-            onClick={() => setCases('deaths')}
-            title="Deaths"
-            cases={country.todayDeaths}
-            total={country.deaths}
-            isRed
-            active={cases === 'deaths'}
-          />
-        </div>
+        <CovidStatistics cases={cases} onSetCases={setCasesState} />
         <Map casesType={cases} />
       </div>
       <CovidInformation cases={cases} />
---
+++ src/components/CovidStatistics/CovidStatistics.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getCountry } from '../../store/selectors/covidSelectors';
+import InfoBox from '../InfoBox/InfoBox';
+import { Cases, IStateCountry, ICountry } from '../../types';
+
+interface ICovidStatisticsProps {
+  cases: Cases;
+  onSetCases: (cases: Cases) => void;
+}
+
+const CovidStatistics: React.FC<ICovidStatisticsProps> = ({
+  cases,
+  onSetCases,
+}) => {
+  const country = useSelector<IStateCountry, ICountry>(getCountry);
+
+  return (
+    <div className="App__stats">
+      <InfoBox
+        onClick={() => onSetCases('cases')}
+        title="Coronavirus Cases"
+        cases={country.todayCases}
+        total={country.cases}
+        isRed
+        active={cases === 'cases'}
+      />
+      <InfoBox
+        onClick={() => onSetCases('recovered')}
+        title="Recovered"
+        cases={country.todayRecovered}
+        total={country.recovered}
+        active={cases === 'recovered'}
+      />
+      <InfoBox
+        onClick={() => onSetCases('deaths')}
+        title="Deaths"
+        cases={country.todayDeaths}
+        total={country.deaths}
+        isRed
+        active={cases === 'deaths'}
+      />
+    </div>
+  );
+};
+
+export default CovidStatistics;
